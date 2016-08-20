077d892d27545f92607fc8b6b2a7bb43f4008f86
jeremy payne <jeremyp@perfectomobile.com>
2016-08-20 00:20:25 -0400

    Initial Commit

---
+++ PerfectoSplunk-ReportingSample/src/test/java/utilities/ClassHelper.java
@@ -0,0 +1,149 @@
+package utilities;
+
+import java.awt.Desktop;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URI;
+
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.ITestContext;
+import org.testng.ITestResult;
+import org.testng.Reporter;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
+
+import com.perfecto.splunk.ReportingCollectorFactory;
+import com.perfecto.splunk.ReportingFactory;
+import com.perfecto.splunk.SplunkReporting;
+import com.perfecto.splunk.SplunkReportingCollector;
+
+
+public abstract class ClassHelper {
+	private RemoteWebDriverExtended driver;
+	public Library lib;
+	private TestSetup tes;
+
+	// Call this at the start of test to set the reporting class and
+	// define splunk details
+	// Params
+	// @1SLA in milliseconds - this will define pass/fail threshold for steps
+	// //based on response time
+	// @2Splunk host
+	// @3Splunk port
+	// @4Splunk scheme http or https
+	// @5Splunk token if required
+	// @4Splunk username
+	// @5Splunk password
+	public void setSplunk() {
+		SplunkReporting reporting = ReportingFactory.createInstance(1000, "52.70.207.153", 8089, "https", "", "admin",
+				"splunkDemo1234");
+		ReportingFactory.setReporting(reporting);
+	}
+
+	// Returns the reportingCollector class for use
+	public SplunkReporting getCollector() {
+		return ReportingFactory.getReporting();
+	}
+
+	@BeforeSuite
+	public void beforeSuite() {
+		setSplunk();
+	}
+
+	@AfterSuite
+	public void afterSuite() {
+		try {
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	@Parameters({ "targetEnvironment" })
+	@BeforeMethod(alwaysRun = true)
+	public void beforeMethod(Method method, String targetEnvironment) {
+
+		setSplunk();
+		// initializes testSetup class
+		tes = new TestSetup(targetEnvironment, driver);
+		// sets up the testNG flows based on testsuite.xml
+		tes.flowControl();
+
+		try {
+			lib = tes.driverAndLibrarySetup();
+		} catch (UnsupportedEncodingException | MalformedURLException | InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		String testName = method.getDeclaringClass().getSimpleName() + "::" + method.getName();
+
+		// Sets the start time of the test
+		getCollector().testExecutionStart();
+
+		// Put any value into the reporting json by passing
+		// Params
+		// @1 parameter name
+		// @2 parameter value
+		getCollector().reporting.put("seleniumHost", tes.host);
+		getCollector().reporting.put("threadNo",
+				Thread.currentThread().getName() + " " + Thread.currentThread().getId());
+
+		lib.setTestName(method.getName());
+	}
+
+	@AfterMethod(alwaysRun = true)
+	public void afterMethod(Method method, ITestResult testResult, ITestContext tcc) throws Exception {
+		try {
+
+			if (lib.isDevice()) {
+
+				lib.getDriver().close();
+
+				getCollector().reporting.put("executionID",
+						(String) lib.getDriver().getCapabilities().getCapability("executionId"));
+				getCollector().reporting.put("reportKey",
+						(String) lib.getDriver().getCapabilities().getCapability("reportKey"));
+				getCollector().reporting.put("perfectoReport",
+						"https://" + tes.host + "/nexperience/Report.html?reportId=SYSTEM%3Adesigns%2Freport&key="
+								+ (String) lib.getDriver().getCapabilities().getCapability("reportKey").toString()
+										.replace(".xml", "")
+								+ "%2Exml&liveUrl=rtmp%3A%2F%2F" + tes.host.replace(".", "%2E")
+								+ "%2Fengine&appUrl=https%3A%2F%2F" + tes.host.replace(".", "%2E") + "%2Fnexperience");
+				getCollector().reporting.put("windTunnelReport",
+						(String) lib.getDriver().getCapabilities().getCapability("windTunnelReportUrl"));
+
+				if (Desktop.isDesktopSupported()) {
+					Desktop.getDesktop().browse(
+							new URI((String) lib.getDriver().getCapabilities().getCapability("windTunnelReportUrl")));
+				}
+			} else {
+				lib.getDriver().quit();
+			}
+		} catch (Exception ex) {
+
+		}
+
+		// Submits the data to splunk
+		// Params
+		// @1 Splunk index name
+		// @2 High level test name - first node in JSON
+		// @3 test case name
+		// return type is String and contains the Json which was written to
+		// Splunk
+		String value = getCollector().commitSplunk("Perfecto", testResult.getMethod().getMethodName(), "test_index");
+		System.out.println(value);
+
+		try {
+			lib.getDriver().quit();
+
+		} catch (Exception ex) {
+
+		}
+	}
+}
---
+++ PerfectoSplunk-ReportingSample/src/test/java/utilities/TestListener.java
@@ -0,0 +1,161 @@
+package utilities;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Date;
+
+import org.testng.IInvokedMethod;
+import org.testng.IInvokedMethodListener;
+import org.testng.IInvokedMethodListener2;
+import org.testng.ISuite;
+import org.testng.ISuiteListener;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestNGMethod;
+import org.testng.ITestResult;
+import org.testng.Reporter;
+import org.testng.TestListenerAdapter;
+import org.testng.internal.thread.IExecutor;
+import org.testng.internal.thread.ThreadUtil;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.perfecto.splunk.ReportingFactory;
+import com.perfecto.splunk.SplunkReporting;
+import com.perfecto.splunk.SplunkReportingCollector;
+
+public class TestListener implements ISuiteListener, ITestListener, IInvokedMethodListener {
+
+	@Override
+	public void beforeInvocation(IInvokedMethod method, ITestResult testResult) {
+	}
+
+	@Override
+	public void afterInvocation(IInvokedMethod method, ITestResult arg0) {
+	}
+
+	@Override
+	public void onFinish(ISuite arg0) {
+	}
+
+	@Override
+	public void onStart(ISuite arg0) {
+	}
+
+	@Override
+	public void onFinish(ITestContext arg0) {
+	}
+
+	@Override
+	public void onStart(ITestContext arg0) {
+	}
+
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult arg0) {
+	}
+	
+	@Override
+	public void onTestStart(ITestResult result) {
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult arg0) {
+		setDetails("Pass", arg0);
+	}
+
+	@Override
+	public void onTestFailure(ITestResult arg0) {
+		String result = "Fail";
+		setDetails(result, arg0);
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult arg0) {
+		setDetails("Skip", arg0);
+	}
+
+	public void setDetails(String result, ITestResult testResult) {
+		// gets the instance of the ClassHelper associated with the thread
+		Object currentClass = testResult.getMethod().getInstance();
+		ClassHelper classHelper = ((ClassHelper) currentClass);
+
+		// Initializes Splunk in case of Skip
+		if (result.equalsIgnoreCase("Skip")) {
+			classHelper.setSplunk();
+		}
+
+		// Creates local instance of SplunkReporting for the listener
+		SplunkReporting splunkReport = classHelper.getCollector();
+
+		splunkReport.reporting.put("testStatus", result);
+
+		splunkReport.reporting.put("className", testResult.getMethod().getInstance().getClass().getName());
+
+		try {
+			splunkReport.reporting.put("hostName", InetAddress.getLocalHost().getHostName());
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		if (!result.equalsIgnoreCase("Skip")) {
+			if (classHelper.lib.isDevice()) {
+				splunkReport.reporting.put("model",
+						(String) classHelper.lib.driver.getCapabilities().getCapability("model"));
+				splunkReport.reporting.put("device",
+						(String) classHelper.lib.driver.getCapabilities().getCapability("deviceName"));
+				splunkReport.reporting.put("os",
+						(String) classHelper.lib.driver.getCapabilities().getCapability("platformName"));
+			} else {
+				splunkReport.reporting.put("device",
+						(String) classHelper.lib.driver.getCapabilities().getPlatform().family().name());
+				splunkReport.reporting.put("os",
+						(String) classHelper.lib.driver.getCapabilities().getPlatform().name());
+				splunkReport.reporting.put("browser",
+						(String) classHelper.lib.driver.getCapabilities().getBrowserName());
+				splunkReport.reporting.put("browserVersion",
+						(String) classHelper.lib.driver.getCapabilities().getVersion());
+			}
+
+		}
+
+		if (result.equalsIgnoreCase("Fail")) {
+			if (testResult.getThrowable() != null)
+				if (testResult.getThrowable().getStackTrace() != null) {
+					StringWriter sw = new StringWriter();
+					testResult.getThrowable().printStackTrace(new PrintWriter(sw));
+					splunkReport.reporting.put("stackTrace", sw.toString());
+				}
+		}
+
+		if (!result.equalsIgnoreCase("Skip")) {
+			// Sets the end time of the test
+			splunkReport.testExecutionEnd();
+		}
+
+		splunkReport.reporting.put("testName", testResult.getTestContext().getName());
+		splunkReport.reporting.put("methodName", testResult.getMethod().getMethodName());
+
+		if (result.equalsIgnoreCase("Skip")) {
+			// setting this here in case of skip
+			// Submits the data to splunk
+			// Params
+			// @1 Splunk index name
+			// @2 High level test name - first node in JSON
+			// @3 test case name
+			// return type is String and contains the Json which was written to
+			// Splunk
+			String value;
+			try {
+				value = splunkReport.commitSplunk("Perfecto", testResult.getMethod().getMethodName(), "test_index");
+				System.out.println(value);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+	}
+}
